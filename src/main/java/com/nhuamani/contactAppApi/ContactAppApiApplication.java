7a2f37e79d015f07e54e66b6d507a6253a545064
nhuamani <nhuamani.c7@gmail.com>
2024-07-09 18:54:19 -0500

    project initiation

---
+++ src/main/java/com/nhuamani/contactAppApi/ContactAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhuamani.contactAppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhuamani/contactAppApi/ContactAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuamani.contactAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
